bb434874a2e66716adec5f9becbbf19cb6aba473
Anishka Sinha <anishkaisstar@gmail.com>
2024-05-28 12:34:06 +0530

    Add project files.

---
+++ BGHAmbulance/Controllers/ADMINsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BGHAmbulance;
+
+namespace BGHAmbulance.Controllers
+{
+    public class ADMINsController : Controller
+    {
+        private BGHAmbulanceEntities db = new BGHAmbulanceEntities();
+
+        // GET: ADMINs
+        public ActionResult Index()
+        {
+            return View(db.ADMINs.ToList());
+        }
+
+        // GET: ADMINs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ADMIN aDMIN = db.ADMINs.Find(id);
+            if (aDMIN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aDMIN);
+        }
+
+        // GET: ADMINs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ADMINs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NAME,CONTACT,EMAIL")] ADMIN aDMIN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ADMINs.Add(aDMIN);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aDMIN);
+        }
+
+        // GET: ADMINs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ADMIN aDMIN = db.ADMINs.Find(id);
+            if (aDMIN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aDMIN);
+        }
+
+        // POST: ADMINs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NAME,CONTACT,EMAIL")] ADMIN aDMIN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aDMIN).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aDMIN);
+        }
+
+        // GET: ADMINs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ADMIN aDMIN = db.ADMINs.Find(id);
+            if (aDMIN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aDMIN);
+        }
+
+        // POST: ADMINs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ADMIN aDMIN = db.ADMINs.Find(id);
+            db.ADMINs.Remove(aDMIN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BGHAmbulance/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BGHAmbulance.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Revolutionizing modern living with innovative, convenient smart solutions";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Get In Touch With Us";
+
+            return View();
+        }
+    }
+}
